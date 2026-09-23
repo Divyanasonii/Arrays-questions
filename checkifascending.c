9cb69425d2563b5f67b062a7cb3bc318362a2dbd
Divyanasonii <maahi.divyana@gmail.com>
2026-09-23 16:40:46 +0530

    made few changes

--- checkifascending.c
+++ checkifascending.c
@@ -6,23 +6,15 @@ int main () {
         scanf("%d", &arr[i]);
     }
 
-    int ascending = 1;
-
    for(int i = 0; i<5; i++){
     for(int j = i +1; j<5; j++){
-        if(arr[i]>arr[j])
-        ascending=0;
-        break;
+        if(arr[i]>arr[j]){
+            printf("Not in ascending order");
+            return 0;
+        }
     }
-    if(ascending==0)
-    break;
    }
-   if(ascending){
-    printf("array is in ascending order");
-   }
-   else {
-    printf("array is not in ascending order");
-   }
+   printf("in ascending order");
 
    return 0;
 }
