46a35716959e01e800b4bf7e42ddbbca89895d58
Divyanasonii <maahi.divyana@gmail.com>
2026-09-26 09:51:47 +0530

    made changes

--- frequencyofeachelement.c
+++ frequencyofeachelement.c
@@ -1,30 +1,28 @@
 #include <stdio.h>
-
 int main() {
-    int arr[6];
-    int visited[6] = {0};
 
-    printf("Enter 6 elements: ");
-
-    for(int i = 0; i < 6; i++) {
-        scanf("%d", &arr[i]);
-    }
-
-    for(int i = 0; i < 6; i++) {
-
-        if(visited[i] == 1)
-            continue;
-
+    int arr[6] = {2, 3, 2, 5, 3, 2};
+    for (int i = 0; i < 6; i++) {
         int count = 1;
-
-        for(int j = i + 1; j < 6; j++) {
-            if(arr[i] == arr[j]) {
-                count++;
-                visited[j] = 1;
+        int already = 0;
+        for (int k = 0; k < i; k++) {
+            if (arr[i] == arr[k]) {
+                already = 1;
+                break;
             }
         }
 
-        printf("%d occurs %d times\n", arr[i], count);
+        if (already == 1)
+            continue;
+
+        // Count frequency
+        for (int j = i + 1; j < 6; j++) {
+            if (arr[i] == arr[j]) {
+                count++;
+            }
+        }
+
+        printf("%d = %d times\n", arr[i], count);
     }
 
     return 0;
